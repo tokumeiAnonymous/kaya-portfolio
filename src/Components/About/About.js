ddb3dc68aabb999d2a3244d98c13bd34bf9258f2
jerome <jerometagubanoone@gmail.com>
2022-06-05 21:51:06 +0800

    Restructured content

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,39 @@ import { AboutStyled } from './About.styled';
 export default function About() {
   return (
     <AboutStyled id='about'>
+      <h2>
+        WHO I AM
+      </h2>
+      <h1>
+        A Bit About Me...
+      </h1>
+      <p>
+        Hello, My name is Jerome Taguba.
+        I am a Mechanical Engineer by profession.
 
+        I have created multiple projects.
+        Check it out below.
+      </p>
+      <div className="skills">
+        <h2>MY SKILLS</h2>
+        <h1>
+          What My Programming Skills Includes?
+        </h1>
+        <div>
+          <i className="devicon-html5-plain-wordmark colored"></i>
+          <i className="devicon-css3-plain colored"></i>
+          <i class="devicon-javascript-plain colored"></i>
+          <i className="devicon-nodejs-plain colored"></i>
+          <i className="devicon-react-original-wordmark colored"></i>
+          <i className="devicon-github-original-wordmark colored"></i>
+          <i className="devicon-git-plain-wordmark colored"></i>
+          <i className="devicon-java-plain-wordmark colored"></i>
+          <i className="devicon-firebase-plain-wordmark colored"></i>
+          <i className="devicon-mongodb-plain-wordmark colored"></i>
+          <i className="devicon-vscode-plain colored"></i>
+          <i className="devicon-blender-original-wordmark colored"></i>
+        </div>
+      </div>
     </AboutStyled>
   )
 }
--- src/Components/About/About.styled.js
+++ src/Components/About/About.styled.js
@@ -1,5 +1,33 @@
 import styled from 'styled-components';
 
 export const AboutStyled = styled.section`
-    background-color: lightblue;
+    display: flex;
+    flex-direction: column;
+    background-color: ${ ({theme}) => theme.colors.main };
+    color: white;
+    justify-content: center;
+
+    h2 {
+        font-weight: 500;
+        color: ${ ({theme}) => theme.colors.yellow};
+        background-color: ${ ({theme}) => theme.colors.secondary};
+        padding: 0.5rem 1rem;
+        border-radius: 1rem;
+        width: fit-content;
+    }
+
+    h1 {
+        margin: 0.5rem;
+        margin-top: 0;
+    }
+
+    p {
+        margin: 0.5rem;
+        font-size: 1.5rem;
+        text-align: justify;
+    }
+
+    .skills div i {
+        font-size: 2rem;
+    }
 `
